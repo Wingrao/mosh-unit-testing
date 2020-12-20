2662f5343ef317ee55139a1970b9e9a31b1a2a4c
Wingrao <56922415+Wingrao@users.noreply.github.com>
2020-12-20 14:56:27 +0200

    Installed Moq

---
+++ TestNinja.UnitTest/Mocking/VideoServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTest.Mocking
+{
+    [TestFixture]
+    class VideoServiceTests
+    {
+        [Test]
+        public void ReadVideoFile_EmptyFile_ReturnError()
+        {
+            var service = new VideoService();
+
+            var result = service.ReadVideoTitle(new FakeFileReader());
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
