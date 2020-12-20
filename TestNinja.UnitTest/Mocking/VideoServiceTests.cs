66f93bbe38a68f3cd03e9be5ad76ec38a20337ea
Wingrao <56922415+Wingrao@users.noreply.github.com>
2020-12-20 17:52:34 +0200

    Finished Module 5

--- TestNinja.UnitTest/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTest/Mocking/VideoServiceTests.cs
@@ -1,17 +1,33 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using TestNinja.Mocking;
 
 namespace TestNinja.UnitTest.Mocking
 {
     [TestFixture]
-    class VideoServiceTests
+    internal class VideoServiceTests
     {
+        private VideoService _service;
+        private Mock<IFileReader> _fileReader;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fileReader = new Mock<IFileReader>();
+            _service = new VideoService();
+        }
+
+
         [Test]
         public void ReadVideoFile_EmptyFile_ReturnError()
         {
-            var service = new VideoService();
+            
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
 
-            var result = service.ReadVideoTitle(new FakeFileReader());
+
+            var result = _service.ReadVideoTitle(_fileReader.Object);
+
+
 
             Assert.That(result, Does.Contain("error").IgnoreCase);
         }
